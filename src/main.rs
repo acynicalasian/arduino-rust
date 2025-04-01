d87c3ac83e69cf9d4dbe4fc6736b89c9ce42b1c4
Arthur Kim <arthur.kim@g.ucla.edu>
2025-04-01 15:12:37 -0700

    Some sort of memory leak or error is occurring when we access SRAM during the setup phase, leading to println to access OOB data during println() statement.

--- src/main.rs
+++ src/main.rs
@@ -59,6 +59,7 @@ fn main() -> ! {
     // Testing... Are we reading properly from registers?
     // We might need to prevent interrupts here to make sure our register reads
     // are completely synchronous.
+    
     interrupt::free(|_| {
         unsafe {
             asm!(
@@ -67,12 +68,13 @@ fn main() -> ! {
             );
             let r16 = 0x10 as *const usize;
             // We expect to output 69.
-            println!("r16 is {}", *r16);
-            println!("value in SRAM: {}", *(0x08f7 as *const usize) as u8);
+            println!("r16 is {}", *r16 as u8);
+            //println!("value in SRAM: {}", (*(0x08f7 as *const usize)) as u8);
+            println!("unsafe print with no deref fine?");
         }
     });
 
-    //println!("testing before fuse bit block");
+    println!("testing before fuse bit block");
 
     // We'll load the fuse high byte into r0 first to get the BOOTSZ fuse bits.
     // SPMCSR IO register cannot be accessed directly by SBIS instructions, and
@@ -85,28 +87,28 @@ fn main() -> ! {
     // Load our calculated value into SPMCSR, thereby setting the BLBSET and
     // SELFPRGEN bits.
     // Run an LPM instruction to load the fuse high bits into r0.
-    unsafe {
-        asm!(
-            "in r16, 0x37",
-            "ori r16, 7",
-            "ldi r30, 3",
-            "ldi r31, 0",
-            "out 0x37, r16",
-            "lpm",
-        );
-    }
+    // unsafe {
+    //     asm!(
+    //         "in r16, 0x37",
+    //         "ori r16, 7",
+    //         "ldi r30, 3",
+    //         "ldi r31, 0",
+    //         "out 0x37, r16",
+    //         "lpm",
+    //     );
+    // }
 
     //println!("testing after fuse bit block");
 
-    let r0 = 0 as *const u16;
-    
     let mut led = pins.d13.into_output();
 
     //println!("testing right before loop");
 
     loop {
         println!("testing in loop");
+        println!("printing after OOB memory access");
         led.toggle();
+        println!("printing after toggling LED");
         arduino_hal::delay_ms(1000);
     }
 }
